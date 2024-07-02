c7adf27f1369584bae7b361ad6dadf76224b8cdc
Aira Jena <airajena.hack@gmail.com>
2024-07-02 23:28:13 +0530

    feat: Add dummy chat data and configure dotenv
    - Added dummy chat data.
    - Configured dotenv for environment variables.
    - Added API endpoint to fetch chat by ID.
    - Ensured TypeScript type safety for request handlers.

---
+++ server/data/data.ts
@@ -0,0 +1,109 @@
+export const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Sounak Dutta",
+          email: "sounak@example.com",
+        },
+        {
+          name: "Aira",
+          email: "aira@example.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName: "Sounak Dutta",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Sounak Dutta",
+          email: "sounak@example.com",
+        },
+        {
+          name: "Aira",
+          email: "aira@example.com",
+        },
+      ],
+      _id: "617a077e18c25468b27c4dd4",
+      chatName: "Sounak Bose",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Dev Ashrit",
+          email: "dev@example.com",
+        },
+        {
+          name: "Aira",
+          email: "aira@example.com",
+        },
+      ],
+      _id: "617a077e18c2d468bc7c4dd4",
+      chatName: "Dev Ashrit",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Ashan Zamil",
+          email: "ashan@example.com",
+        },
+        {
+          name: "Aira",
+          email: "aira@example.com",
+        },
+        {
+          name: "Rahul Barik",
+          email: "rahul@example.com",
+        },
+      ],
+      _id: "617a518c4081150716472c78",
+      chatName: "Strugglers",
+      groupAdmin: {
+        name: "Rahul Barik",
+        email: "rahul@example.com",
+      },
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Priyam ",
+          email: "priyam@example.com",
+        },
+        {
+          name: "Aira",
+          email: "aira@example.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7cfdd4",
+      chatName: "Priyam ",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Sai Kanha",
+          email: "sai@example.com",
+        },
+        {
+          name: "Aira",
+          email: "aira@example.com",
+        },
+        {
+          name: "Emee ",
+          email: "emee@example.com",
+        },
+      ],
+      _id: "617a518c4081150016472c78",
+      chatName: "GroundZero",
+      groupAdmin: {
+        name: "Emee ",
+        email: "emee@example.com",
+      },
+    },
+  ];
+  
---
+++ server/server.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import { chats } from "./data/data";
+import dotenv from "dotenv";
+
+const PORT= process.env.PORT || 3000;
+const app = express();
+dotenv.config();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("API is running");
+});
+
+app.get("/api/chats", (req: Request, res: Response) => {
+    res.send(chats);
+});
+
+app.get("/api/chats/:id", (req: Request, res: Response) => {
+    const chat = chats.find((c) => c._id === req.params.id);
+    res.send(chat);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
